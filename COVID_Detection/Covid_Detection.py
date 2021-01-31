764633cb9d49a480f6eadbd5d296a61f6f66a919
Suraj Biswas <reetbiswas503@gmail.com>
2021-01-31 21:24:44 +0530

    Code for covid detection

---
+++ COVID_Detection/Covid_Detection.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+import pandas as pd
+import Get_Data as Data
+
+dataset_path = './Dataset/'
+
+def CreateDirectory(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+if __name__ == "__main__":
+    #Create some directory
+    CreateDirectory(dataset_path + 'CT_Images/')
+    CreateDirectory(dataset_path + 'CT_Images/' + 'Train')
+    CreateDirectory(dataset_path + 'CT_Images/' + 'Test')
+
+    ImagePaths = ["Chest_Xray/","Covid_Xray/"]
+    md = Data.MakeDataset(ImagePaths)
+    md.TrainData()
+    
